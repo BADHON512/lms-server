8d7f1dc891a85dfeb409642a760eb6cbed2c2d5e
badhon <mylaptop512@gmail.com>
2023-11-25 22:23:19 +0600

    done but not at all

--- routes/layout.router.ts
+++ routes/layout.router.ts
@@ -1,16 +1,23 @@
-import { EditLayout, getLayout } from './../controllers/layout.controller';
+import { EditLayout, getLayout } from "./../controllers/layout.controller";
 import { authorizeRoles } from "./../controllers/userController";
 import express from "express";
 import { isAuthenticated } from "../middleware/auth";
 import { createLayout } from "../controllers/layout.controller";
 
+const FaqRouter = express.Router();
 
-
-
-const FaqRouter= express.Router();
- 
-FaqRouter.post("/create-layout",isAuthenticated,authorizeRoles('admin'),createLayout);           
-FaqRouter.put("/edit-layout",isAuthenticated,authorizeRoles('admin'),EditLayout);           
-FaqRouter.get("/get-layout",getLayout);           
+FaqRouter.post(
+  "/create-layout",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  createLayout
+);
+FaqRouter.put(
+  "/edit-layout",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  EditLayout
+);
+FaqRouter.get("/get-layout", getLayout);
 
 export default FaqRouter;
