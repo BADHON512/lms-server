864861fc394ec9fe783956715e0adc9bebc67052
badhon <mylaptop512@gmail.com>
2023-12-09 18:27:59 +0600

    post replace by put

--- routes/user.route.ts
+++ routes/user.route.ts
@@ -14,7 +14,7 @@ userRouter.post('/social-auth',socialAuth)
 userRouter.put('/update-user-info',isAuthenticated,updateUserInfo)
 userRouter.put('/update-profile',isAuthenticated,updateProfilePicture)
 userRouter.put('/update-user-role',isAuthenticated,authorizeRoles('admin') ,updateUserRole)
-userRouter.post('/update-password',isAuthenticated,updatePassword)
+userRouter.put('/update-password',isAuthenticated,updatePassword)
 userRouter.get('/get-all-users',isAuthenticated,authorizeRoles('admin'),getAllUsers)
 userRouter.get('/delete-users/:id',isAuthenticated,authorizeRoles('admin'),deleteUser)
 export default userRouter
